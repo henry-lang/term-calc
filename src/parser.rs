c9b3dfa9eb171862ac5162ae058ffaee00058606
Henry Langmack <Henry.langmack@gmail.com>
2022-03-21 19:57:15 -0400

    lots of parsing code added

---
+++ src/parser.rs
@@ -0,0 +1,140 @@
+use crate::tokenizer::{Token, TokenType};
+use TokenType::*;
+
+/*
+GRAMMAR:
+expr: term ((+ | -) term)*
+term: factor ((* | /) factor)*
+factor: NUMBER |
+        OPENPAREN expr CLOSEPAREN |
+        (-) factor
+
+*/
+#[derive(Debug)]
+pub enum UnaryOpType {
+    Negate
+}
+
+#[derive(Debug)]
+pub enum BinaryOpType {
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+}
+
+type ChildNode = Box<Node>;
+
+#[derive(Debug)]
+pub enum Node {
+    Number(f64),
+
+    UnaryOp {
+        op_type: UnaryOpType,
+        operand: ChildNode 
+    },
+    
+    BinaryOp {
+        op_type: BinaryOpType,
+        lhs: ChildNode,
+        rhs: ChildNode
+    }
+}
+
+pub struct Parser {
+    current: usize,
+    tokens: Vec<Token>
+}
+    
+impl Parser {
+    pub fn new(tokens: Vec<Token>) -> Self {
+        Self {current: 0, tokens}
+    }
+    
+    pub fn token(&self) -> Token {
+        self.tokens[self.current]
+    }
+
+    fn consume(&mut self, token_type: TokenType) {
+        if token_type == self.token().token_type {
+            if self.current + 1 < self.tokens.len() {
+                self.current += 1
+            }
+        } else {
+            panic!("\x1b[31mExpected {:?}, found {:?}\x1b[0m", token_type, self.token());
+        }
+    }
+    
+    fn get_factor(&mut self) -> Box<Node> {
+        let token_type = self.token().token_type;
+        match token_type {
+            NumLiteral => {
+                let value = self.token().value.unwrap();
+                self.consume(NumLiteral);
+
+                Box::new(Node::Number(value))
+            }
+
+            OpenParen => {
+                self.consume(OpenParen);
+                let expression = self.get_expression();
+                self.consume(CloseParen);
+
+                expression
+            }
+
+            MinusSign => {
+                self.consume(MinusSign);
+                let factor = self.get_factor();
+
+                Box::new(Node::UnaryOp{op_type: UnaryOpType::Negate, operand: factor})
+            }
+
+            _ => { panic!("Expected number or opening parentheses.") }
+        }
+    }
+
+    fn get_term(&mut self) -> Box<Node> {
+        let factor = self.get_factor();
+
+        while [TimesSign, DivideSign].contains(&self.token().token_type) {
+            match self.token().token_type {
+                TimesSign => {
+                    self.consume(TimesSign);
+                    return Box::new(Node::BinaryOp{op_type: BinaryOpType::Multiply, lhs: factor, rhs: self.get_factor()});
+                },
+
+                DivideSign => {
+                    self.consume(DivideSign);
+                    return Box::new(Node::BinaryOp{op_type: BinaryOpType::Divide, lhs: factor, rhs: self.get_factor()});
+                },
+
+                _ => { panic!() }
+            }
+        }
+
+        factor
+    }
+
+    pub fn get_expression(&mut self) -> Box<Node> {
+        let term = self.get_term();
+
+        while [PlusSign, MinusSign].contains(&self.token().token_type) {
+            match self.token().token_type {
+                PlusSign => {
+                    self.consume(PlusSign);
+                    return Box::new(Node::BinaryOp{op_type: BinaryOpType::Add, lhs: term, rhs: self.get_term()})
+                },
+
+                MinusSign => {
+                    self.consume(MinusSign);
+                    return Box::new(Node::BinaryOp{op_type: BinaryOpType::Subtract, lhs: term, rhs: self.get_term()})
+                },
+
+                _ => { panic!() }
+            }
+        }
+
+        term
+    }
+}
---
+++ src/tokenizer.rs
@@ -0,0 +1,63 @@
+#[derive(Copy, Clone, PartialEq, Debug)]
+pub enum TokenType {
+    PlusSign,
+    MinusSign,
+    TimesSign,
+    DivideSign,
+    
+    OpenParen,
+    CloseParen,
+
+    NumLiteral
+}
+
+use TokenType::*;
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub value: Option<f64>
+}
+
+impl Token {
+    fn new(token_type: TokenType) -> Self {
+        Self {token_type, value: None}
+    }
+
+    fn new_with_value(token_type: TokenType, value: f64) -> Self {
+        Self {token_type, value: Some(value)}
+    }
+}
+
+pub fn tokenize(expression: &String) -> Option<Vec<Token>> {
+    let filtered = expression.split_whitespace().collect::<String>();
+    let mut tokens = Vec::<Token>::new();
+    
+    let mut iter = filtered.chars().enumerate().peekable();
+    while let Some((idx, c)) = iter.next() {
+        if c.is_numeric() {
+            let mut end_idx = idx + 1;
+            while let Some(_) = iter.next_if(|(_, next)| next.is_numeric() || *next == '.') {
+                end_idx += 1;
+            }
+    
+            let value = filtered[idx..end_idx]
+                .parse::<f64>()
+                .expect("number is valid");
+            
+            tokens.push(Token::new_with_value(NumLiteral, value));
+        } else {
+            tokens.push(match c {
+                '+' => Token::new(PlusSign),
+                '-' => Token::new(MinusSign),
+                '*' => Token::new(TimesSign),
+                '/' => Token::new(DivideSign),
+                '(' => Token::new(OpenParen),
+                ')' => Token::new(CloseParen),
+                _ => continue
+            });
+        }
+    }
+    
+    Some(tokens)
+}
