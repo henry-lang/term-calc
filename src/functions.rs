db04b761a125a60d5615dbf8ee5bf492a0fcbba0
Henry Langmack <Henry.langmack@gmail.com>
2022-03-23 12:11:42 -0400

    add functions

--- src/functions.rs
+++ src/functions.rs
@@ -9,6 +9,11 @@ pub fn get_registry() -> FunctionRegistry {
     registry.insert("sin".into(), f64::sin);
     registry.insert("cos".into(), f64::cos);
     registry.insert("tan".into(), f64::tan);
+    registry.insert("arcsin".into(), f64::asin);
+    registry.insert("arccos".into(), f64::acos);
+    registry.insert("arctan".into(), f64::atan);
+    registry.insert("sqrt".into(), f64::sqrt);
+    registry.insert("ln".into(), f64::ln);
 
     registry
 }
